ffec409d17bef2f456ab826a325a700ed0b2aa2c
vinay <vinay@gmail.com>
2019-03-12 16:39:16 +0000

    initial commit for system-info.sh

---
+++ system-info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this script to demonstarate system info
+
+echo ###################################
+echo 
+echo "Disk Usage"
+
+df -h 
+
+
+echo #####################
+
+echo "memory available"
+
+free -m
+
